294fd66f97c497837c093d73483e5ee898871860
Michael Forney <mforney@mforney.org>
2021-09-23 16:29:55 -0700

    rv64: fix slot address calculation with large offset

--- rv64/emit.c
+++ rv64/emit.c
@@ -230,6 +230,7 @@ fixslot(Ref *pr, Fn *fn, FILE *f)
 		s = slot(r.val, fn);
 		if (s < -2048) {
 			fprintf(f, "\tli t6, %"PRId64"\n", s);
+			fprintf(f, "\tadd t6, fp, t6\n");
 			*pr = TMP(T6);
 		}
 	}
